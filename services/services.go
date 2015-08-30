fb572cbce4d680a10aaf058c946ced686e4af748
Merten van Gerven <merten.vg@gmail.com>
2015-08-30 17:36:24 +0200

    Added Has method

--- services/services.go
+++ services/services.go
@@ -31,7 +31,13 @@ func Register(name string, service Service) {
 	mutex.Unlock()
 }
 
-// Get a service by name
+// Has checks for the existence of a service by name
+func Has(name string) bool {
+	_, ok := services[name]
+	return ok
+}
+
+// Get a service by name from the registry
 func Get(name string) (Service, bool) {
 	if s, ok := services[name]; ok {
 		return s.init(), true
@@ -39,7 +45,7 @@ func Get(name string) (Service, bool) {
 	return nil, false
 }
 
-// GetInto loads a service into the given pointer by name
+// GetInto loads a service into the given pointer by name from the registry
 func GetInto(name string, into Service) bool {
 	s, ok := Get(name)
 	into = s
